cdb65f9bde5f80a19d790a07fc000323263c52b7
Dmytro Burtsev <burtsev@air-dmytro.home>
2026-05-25 21:01:48 +0200

    Fix typo in Scarlett Keegan name and remove extra space in Actor model

--- db/models.py
+++ db/models.py
@@ -8,7 +8,7 @@ class Genre(models.Model):
         return self.name
 
 
-class Actor (models.Model):
+class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
